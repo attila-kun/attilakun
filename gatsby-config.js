fa009a4501b9c55e1116401cd4af7b7912c32a81
Attila Kun <kun.attila.hun@gmail.com>
2021-01-15 00:28:09 +0000

    Added personal page template.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,55 @@
 module.exports = {
   siteMetadata: {
-    title: "My Gatsby Site",
+    title: `John Doe`,
+    author: `John Doe`,
+    firstName: `John`,
+    lastName: `Doe`,
+    description: `John Doe's personal site`,
+    occupation: `Software Engineer`,
+    keywords: [`John`, `Doe`, `Personal`, `Blog`, `Resume`, `Projects`, `Work`],
+    siteUrl:
+      process.env.URL || process.env.DEPLOY_URL || `http://localhost:8000`,
+    unemployed: true,
+    designations: [
+      `Coding Monkey`,
+      `Jedi Master`,
+      `Binary Sunset Enthusiast`,
+      `Moisture Farmer`,
+    ],
+    readingList: [
+      {
+        title: `Sapiens: A Brief History of Humankind`,
+        author: `Yuval Noah Harari`,
+        link: `https://www.goodreads.com/book/show/23692271-sapiens`,
+      },
+      {
+        title: `Behave: The Biology of Humans at our Best and Worst`,
+        author: `Robert Sapolsky`,
+        link: `https://www.goodreads.com/book/show/31170723-behave`,
+      },
+      {
+        title: `The Gene: An Intimate History`,
+        author: `Siddhartha Mukeherjee`,
+        link: `https://www.goodreads.com/book/show/27276428-the-gene`,
+      },
+    ],
+    showsList: [
+      {
+        title: `Avatar: The Last Airbender`,
+        author: `Micheal DiMartino, Bryan Konietzko`,
+        link: `https://www.imdb.com/title/tt0417299/`,
+      },
+      {
+        title: `Love, Death & Robots`,
+        author: `Tim Miller`,
+        link: `https://www.imdb.com/title/tt9561862/`,
+      },
+      {
+        title: `Star Wars: The Clone Wars`,
+        author: `George Lucas, Dave Filoni`,
+        link: `https://www.imdb.com/title/tt0458290/`,
+      },
+    ],
   },
   plugins: [
     {
@@ -14,5 +63,6 @@ module.exports = {
         anonymize: true
       },
     },
+    `gatsby-plugin-sass`
   ],
 };
