ce9cf74b807f1ec1d9dfb25ab7e052507a4cd389
Attila Kun <kun.attila.hun@gmail.com>
2021-01-17 20:59:34 +0000

    Using Bootstrap 4.5.3. Added navbar. Changed input folder.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({"node_modules/bloch/dist/lib.js": "bloch/bloch.js"});
   return {
     dir: {
+      input: "src",
       output: "public"
     }
   };
